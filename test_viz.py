2067bdb9d0f9947a674cb94d0c988049f3038ea4
Brendan Hayward <brendanhayward85@gmail.com>
2019-07-21 21:04:33 +1000

    create test stubs

---
+++ test_viz.py
@@ -0,0 +1,48 @@
+def test_create_distance_matrix():
+    pass
+
+def test_get_translation_table():
+    pass
+
+
+def test_naive_backtranslate():
+    pass
+
+def test_get_peptide_index():
+    pass
+
+def test_demo_dna_features_viewer():
+    pass
+
+def test_ngrams():
+    pass
+
+def test_make_trigrams():
+    pass
+
+def test_nucleotide_distribution():
+    pass
+
+def test_get_peptide_toplot():
+    pass
+
+def test_peptide_distribution():
+    pass
+
+def test_plot_ABI():
+    pass
+
+def test_get_genbank_sequence():
+    pass
+
+def test_get_fasta_sequence():
+    pass
+
+def test_calc_sequence_similarity():
+    pass
+
+def test_make_parser():
+    pass
+
+def test_main():
+    pass
